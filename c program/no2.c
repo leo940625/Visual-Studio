a5f9da721c1ec6de34daa103cddb52cc3e758fa5
leo940625 <leo940625@gmail.com>
2024-03-05 15:47:18 +0800

    ss

--- c program/no2.c
+++ c program/no2.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <math.h>
 int main()
 {
-    int i = 10;
-    printf("%d",i);
+    int r;
+    const float PI = 3.14;
+    printf("input your radius:");
+    scanf("%d",&r);
+    float volume = 4*PI*pow(r,3)/3;
+    printf("%5.5f",volume);
     return 0;
 }
