4e57cbc8d18723fddc394b6321e5ede1496d055b
leo940625 <leo940625@gmail.com>
2024-03-14 09:33:09 +0800

    ss

---
+++ c program/no3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int a,b,c,d;
+    printf("enter a number between 0 and 32767:");
+    scanf("%d",&a);
+    printf("In octal,your number is:0");
+    for (int i = 3; i >= 0; i--)
+    {
+        b=a/pow(8,i);
+        printf("%d",b);
+        a = a-b*pow(8,i);
+    }
+    return 0;
+}
