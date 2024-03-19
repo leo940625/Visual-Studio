2176de6729ed03a814ab6637cb7ed6f985618ddd
leo940625 <leo940625@gmail.com>
2024-03-19 16:00:06 +0800

    ss

--- c program/test.c
+++ c program/test.c
@@ -2,21 +2,5 @@
 #include <math.h>
 int main()
 {
-    int i = 30;
-    int j = 5;
-    int k = 10;
-    int f;
-    f = i < 20 ? 5 : 10;
-   /*
-   if (i<20)
-   {
-    f = 5;
-   }
-   else
-   {
-    f = 10;
-   }
-   */
-    printf("%d",f);
-    return 0;
+    
 }
