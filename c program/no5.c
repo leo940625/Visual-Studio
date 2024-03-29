902fc83632c6faed58869d1ff6d883e18c9da240
leo940625 <leo940625@gmail.com>
2024-03-29 10:59:31 +0800

    ss

--- c program/no5.c
+++ c program/no5.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include <math.h>
 int main()
 {
-    
+    int a,b,c,d,e,f,g;
+    printf("enter a fraction:");
+    scanf("%d/%d",&a,&b);
+    c = a;
+    d = b;
+     while (a!=0 && b!=0)
+    {
+       if (abs(a)>=abs(b))
+       {
+        a = a%b;
+       }
+       else if (abs(a)<abs(b))
+       {
+        b = b%a;
+       }
+    }
+    g = a==0? b : a;
+    e = c/g;
+    f = d/g;
+    printf("in lowest terms:");
+    printf("%d/%d",e,f);
 }
