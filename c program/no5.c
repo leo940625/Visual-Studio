2840b811113841c00ef6a5a01f7484568aed83a9
leo940625 <leo940625@gmail.com>
2024-04-01 09:06:45 +0800

    ss

--- c program/no5.c
+++ c program/no5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
-int a,b;
-int hcf(int a,int b){
+
+int gcd(int a,int b){
     int x;
     while (a!=0 && b!=0)
     {
@@ -12,16 +12,16 @@ int hcf(int a,int b){
        else if (abs(a)<abs(b))
        {
         b = b%a;
-       } 
+       }
     } 
     x = a==0 ? b : a;
     return x;
 }
 int main(){
-    int e,f,g;
+    int a,b,e,f,g;
     printf("enter a fraction:");
     scanf("%d/%d",&a,&b);
-    g = hcf(a,b);
+    g = gcd(a,b);
     e = a/g;
     f = b/g;
     printf("In lowest terms:");
