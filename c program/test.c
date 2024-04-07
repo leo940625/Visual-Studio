4400f523a7545ecb68d355727676288af4893b87
leo940625 <leo940625@gmail.com>
2024-04-07 22:31:53 +0800

    ss

--- c program/test.c
+++ c program/test.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main()
 {
-    float a = 1097.002;
-    printf("%f",a);
+    int i, k;
+    printf("Please enter the number:");
+    scanf("%d", &i);
+    for (k = i; k > 1; k = k - 1)
+    {
+        int flag = 1, n;
+        for (n = 2; n <= k / 2; n++)
+        {
+            if (k % n == 0)
+            {
+                flag = 0;
+                break;
+            }
+        }
+        if (flag == 1)
+        {
+            printf("%d is a prime under %d \n", k, i);
+        }
+    }
     return 0;
 }
