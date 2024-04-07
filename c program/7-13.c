bfc64a5e5009efea74e0fca7c6e03511afd9827f
leo940625 <leo940625@gmail.com>
2024-04-07 16:10:38 +0800

    s

--- c program/7-13.c
+++ c program/7-13.c
@@ -6,13 +6,13 @@ int main()
     float i = 0;
     float j = 0;
     double g;
-    printf("enter a sentence:");
+    printf("Enter a sentence:");
     do
     {
         scanf("%c",&a);
         a != ' ' ? i++ : j++;
     } while (a != '.');
     g = i/(j+1);
-    printf("average word length:%.1lf",g);
+    printf("Average word length:%.1lf",g);
     return 0;
 }
