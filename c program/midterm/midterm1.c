d2319059981eda21ac2b39770fd5f37a775273e5
leo940625 <leo940625@gmail.com>
2024-04-16 14:32:42 +0800

    ss

--- c program/midterm/midterm1.c
+++ c program/midterm/midterm1.c
@@ -5,13 +5,5 @@ int main()
     printf("Enter a number:");
     scanf("%d",&a);
     a % 2 == 0 ? printf("number is even") : printf("number is odd");
-    /*if (a % 2 == 0)
-    {
-        printf("number is even");
-    }
-    else
-    {
-        printf("number is odd");
-    }*/
     return 0;
 }
