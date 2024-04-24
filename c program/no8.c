4cad318f7ae3207a381dd01bad5eee4020fa29df
leo940625 <leo940625@gmail.com>
2024-04-24 16:56:58 +0800

    ss

---
+++ c program/no8.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+void sort3(int *a,int *b ,int *c){
+    int temp1,temp2;
+    if(*a > *b){
+        temp1 = *a;
+        *a = *b;
+        *b = temp1;
+    }
+    if(*b > *c){
+        temp1 = *b;
+        *b = *c;
+        *c = temp1;
+    }
+    if(*a > * b){
+        temp1 = *a;
+        *a = *b;
+        *b = temp1;
+    }
+}
+int main()
+{
+    int a,b,c;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    sort3(&a,&b,&c);
+    printf("%d,%d,%d",a,b,c);
+}
+
+//謝五兆有多少照
