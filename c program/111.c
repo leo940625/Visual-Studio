cdafa1aba2797c97348bac67d114e4ff196c9ffb
leo940625 <leo940625@gmail.com>
2024-04-24 21:12:54 +0800

    ss

--- c program/111.c
+++ c program/111.c
@@ -1,11 +1,47 @@
 #include <stdio.h>
-void swap(int *a,int *b){
+void swap(int *a, int *b)
+{
     int temp;
     temp = *a;
     *a = *b;
-    *b = temp; 
+    *b = temp;
 }
 int main()
 {
+    int n;
+    printf("Enter number counter:");
+    scanf("%d", &n);
+    int a[n];
+    printf("Enter number:");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for (int j = 0; j < n; j++)
+    {
+        if (j == n - 1)
+        {
+            break;
+        }
+        if (a[j] > a[j + 1])
+        {
+            swap(&a[j],&a[j+1]);
+            j = 0;
+        }
+    }
+    /*
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                swap(&a[j], &a[j + 1]);
+            }
+        }
+    }
+    */
+    printf("Sorted number:");
+    for (int k = 0; k < n; k++)
+    {
+        printf("%d ",a[k]);
+    }
     return 0;
 }
