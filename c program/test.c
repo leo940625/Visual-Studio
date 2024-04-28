743c367cc38f63680095d2f8b7a281d9fc7fcfc9
leo940625 <leo940625@gmail.com>
2024-04-28 23:59:29 +0800

    ss

--- c program/test.c
+++ c program/test.c
@@ -1,29 +1,11 @@
 #include <stdio.h>
-void max_min(int a[], int n, int *max, int *min){
-    *max = *min = a[0];
-    for (int i = 1; i < n; i++)
-    {
-        if (a[i] > *max)
-        {
-            *max = a[i];
-        }
-        if (a[i] < *min)
-        {
-            *min = a[i];
-        }
-    }
-}
+
 int main()
 {
-    int big,small;
-    int a[10];
-    int n = 10;
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    max_min(a,n,&big,&small);
-    printf("Largest:%d\n",big);
-    printf("Smallest:%d\n",small);
+    char a[100];
+    scanf("%s",a);
+    printf("%s",a);
+    size_t n = sizeof(a) / sizeof(a[0]);
+    printf("%d",n);
     return 0;
 }
