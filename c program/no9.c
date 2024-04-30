c3a4f9773708502a771edf9c7d5bcddc3e4c9681
leo940625 <leo940625@gmail.com>
2024-04-30 17:08:30 +0800

    ss

--- c program/no9.c
+++ c program/no9.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+
 int main()
 {
-    char *p = "abc0";
-    char c[10];
-    scanf("%s",c);
-    printf("%d",c[0]);
-    printf("%s",c);
+    char a[20][20];
+    char max[20],min[20];
+    int i = 0;
+    while (1)
+    {
+        printf("Enter word: ");
+        scanf("%s",a[i]);
+        if (strlen(a[i]) == 4)
+        {
+            break;
+        }
+        i++;
+    }
+    strcpy(min,a[0]);
+    strcpy(max,a[0]);
+    for (int j = 0; j <= i; j++)
+    {
+        if (strcmp(a[j],min) < 0)
+        {
+            strcpy(min,a[j]);
+        }
+        else if (strcmp(a[j],max) > 0)
+        {
+            strcpy(max,a[j]);
+        }
+    }
+    printf("The smallest word:%s\n",min);
+    printf("The largest word:%s",max);
+    return 0;
 }
