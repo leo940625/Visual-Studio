86a8eaa181adb2af6c36a474c3508a2cd3b2024f
leo940625 <leo940625@gmail.com>
2024-05-07 19:52:34 +0800

    ss

--- c program/test.c
+++ c program/test.c
@@ -15,14 +15,12 @@ int main()
         x[1].hour += 24;
     }
     last.hour = x[1].hour - x[0].hour;
-    //last.hour = x[0].hour > x[1].hour ? x[1].hour - x[0].hour + 24 : x[1].hour - x[0].hour;  
     if (x[0].minute > x[1].minute)
     {
         x[1].minute += 60;
         last.hour -= 1;
     }
     last.minute = x[1].minute - x[0].minute;
-    //last.minute = x[0].minute > x[1].minute ? x[1].minute - x[0].minute + 60,last.hour -= 1: x[1].minute - x[0].minute;
     if (x[0].sec > x[1].sec)
     {
         x[1].sec += 60;
