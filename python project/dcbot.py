226194a9ad53a84186a6b58b7b34003e96a334b3
leo940625 <leo940625@gmail.com>
2024-05-13 01:07:40 +0800

    ss

--- python project/dcbot.py
+++ python project/dcbot.py
@@ -1,24 +1,22 @@
 # 導入Discord.py模組
 import discord
+# 導入commands指令模組
+from discord.ext import commands
 
-# client是跟discord連接，intents是要求機器人的權限
-intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents = intents)
+# intents是要求機器人的權限
+intents = discord.Intents.all()
+# command_prefix是前綴符號，可以自由選擇($, #, &...)
+bot = commands.Bot(command_prefix = "%", intents = intents)
 
-# 調用event函式庫
-@client.event
+@bot.event
 # 當機器人完成啟動
 async def on_ready():
-    print(f"目前登入身份 --> {client.user}")
+    print(f"目前登入身份 --> {bot.user}")
 
-@client.event
-# 當頻道有新訊息
-async def on_message(message):
-    # 排除機器人本身的訊息，避免無限循環
-    if message.author == client.user:
-        return
-    # 新訊息包含Hello，回覆Hello, world!
-    if message.content == "Hello":
-        await message.channel.send("Hello, world!")
-client.run("機器人的TOKEN")
+@bot.command()
+# 輸入%Hello呼叫指令
+async def Hello(ctx):
+    # 回覆Hello, world!
+    await ctx.send("Hello, world!")
+
+bot.run("機器人的TOKEN")
