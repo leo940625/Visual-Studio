1c7f923b14a7c026334443cf0c1a0129030054b0
leo940625 <leo940625@gmail.com>
2024-05-15 23:14:39 +0800

    ss

--- c program/no11-1.c
+++ c program/no11-1.c
@@ -6,15 +6,14 @@ struct node{
         struct node *next;
 };
 void printList(struct node *a);
-void push(int b,struct node *first,struct node *new);
+void push(int b,struct node **first);
 int main(){
     int a;
     struct node *head = NULL;
-    struct node *new_node;
     printf("Print a list:");
     for (int i = 0; i < 5; i++){
         scanf("%d",&a);
-        push(a,head,new_node);
+        push(a,&head);
     }
     printf("Reversed Linked list:");
     printList(head);
@@ -27,9 +26,10 @@ void printList(struct node *a){
         a = a->next;
     }
 }
-void push(int b,struct node *first,struct node *new){
+void push(int b,struct node **first){
+    struct node *new;
     new = malloc(sizeof (struct node)); // 重新取一個空間
     new->value = b;
-    new -> next = first; //原本指的next空間變成first原本指向的Null
-    first = new; // new head 指到a1
+    new -> next = *first; //原本指的next空間變成first原本指向的Null
+    *first = new; // new head 指到a1
 }
