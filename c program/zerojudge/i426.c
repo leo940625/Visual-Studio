9f24d520598a8a23c09337df4b388448aa53215c
leo940625 <leo940625@gmail.com>
2024-05-21 23:56:20 +0800

    ss

---
+++ c program/zerojudge/i426.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+struct node
+{
+    int value;
+    struct node *next;
+};
+int printList(struct node *a);
+void push(int b, struct node **first);
+void reverseList(struct node **first);
+void insert_node(struct node **first, int insert_after_value, int value);
+int main()
+{
+    struct node *head = NULL;
+    int n, step;
+    scanf("%d", &n);
+    scanf("%d", &step);
+    for (int i = 1; i <= n; i++)
+    {
+        push(i, &head);
+    }
+    reverseList(&head);
+    int a, b, c;
+    for (int j = 0; j < step; j++)
+    {
+        scanf("%d", &a);
+        switch (a)
+        {
+        case 1:
+            /* code */
+            break;
+        case 2:
+            /* code */
+            break;
+        case 3:
+            /* code */
+            break;
+        case 4:
+            reverseList(&head);
+            break;
+        }
+    }
+    return 0;
+}
+int printList(struct node *a)
+{
+    int sum = 0;
+    int i = 1;
+    while (a != NULL)
+    {
+        if (i % 2 != 0)
+        {
+            sum += a->value;
+        }
+        i++;
+        a = a->next;
+    }
+    return sum;
+}
+void push(int b, struct node **first)
+{
+    struct node *new = malloc(sizeof(struct node)); // 重新取一個空間
+    if (new == NULL)
+    {
+        printf("Memory allocation failed");
+        exit(1); // 退出程序
+    }
+    new->value = b;
+    new->next = *first; // 原本指的next空間變成first原本指向的Null
+    *first = new;       // new head 指到a1
+}
+void reverseList(struct node **first)
+{
+    struct node *buffer = NULL, *now = *first, *nextnode;
+    while (now != NULL)
+    {
+        nextnode = now->next;
+        now->next = buffer;
+        buffer = now;
+        now = nextnode;
+    }
+    *first = buffer;
+}
+void insert_node(struct node **first, int insert_after_value, int value)
+{
+    struct node *current = *first;
+    while (current != NULL)
+    {
+        if (insert_after_value == current->value)
+        {
+            struct node *new_node = malloc(sizeof(struct node));
+            new_node->value = value;
+            new_node->next = current->next;
+            current->next = new_node;
+            break;
+        }
+        current = current->next;
+    }
+}
