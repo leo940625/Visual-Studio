9a300d9f8b8074c45583bcc7bdcb26cef1703a58
leo940625 <leo940625@gmail.com>
2024-06-03 10:47:18 +0800

    ss

--- c program/leetcode/876.c
+++ c program/leetcode/876.c
@@ -19,7 +19,49 @@ struct node* middleNode(struct ListNode* head) {
     head = current;
     return head;
 }
+void push(int b, struct node **first)
+{
+    struct node *new = malloc(sizeof(struct node)); // 重新取一個空間
+    if (new == NULL)
+    {
+        printf("Memory allocation failed");
+        exit(1); // 退出程序
+    }
+    new->value = b;
+    new->next = *first; // 原本指的next空間變成first原本指向的Null
+    *first = new;       // new head 指到a1
+}
+void printList(struct node *a)
+{
+    while (a != NULL)
+    {
+        printf("%d ", a->value);
+        a = a->next;
+    }
+}
+void reverseList(struct node **first)
+{
+    struct node *buffer = NULL, *now = *first, *nextnode;
+    while (now != NULL)
+    {
+        nextnode = now->next;
+        now->next = buffer;
+        buffer = now;
+        now = nextnode;
+    }
+    *first = buffer;
+}
 int main()
 {
+    int a;
+    struct node *head = NULL;
+    printf("Print a list:");
+    for (int i = 1; i < 6; i++)
+    {
+        push(i, &head);
+    }
+    reverseList(&head);
+    head = middleNode(head);
+    printList(head);
     return 0;
 }
