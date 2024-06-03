7aba50df7983e395938dd7b0b83f56a8311b3ace
leo940625 <leo940625@gmail.com>
2024-06-03 11:11:32 +0800

    ss

--- c program/leetcode/876.c
+++ c program/leetcode/876.c
@@ -1,32 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 struct node
 {
     int value;
     struct node *next;
 };
-struct node* middleNode(struct node* head) {
-    struct node *current=head;
-    int i;
-    float j;
-    for(i = 1;current != NULL;i++){
-        current=current->next;
-    }
-    current = head;
-    j = i % 2 == 0 ? (i/2)+1 : (i/2)+0.5;
-    for(int k = 1;k != j;k++){
+void middleNode(struct node **head)
+{
+    struct node *current = *head;
+    int count = 0, middle;
+    while (current != NULL)
+    {
+        count++;
         current = current->next;
     }
-    head = current;
-    return head;
+    current = *head;
+    middle = count / 2;
+    for (int i = 0; i < middle; i++)
+    {
+        current = current->next;
+    }
+    *head = current;
 }
 void push(int b, struct node **first)
 {
     struct node *new = malloc(sizeof(struct node)); // 重新取一個空間
-    if (new == NULL)
-    {
-        printf("Memory allocation failed");
-        exit(1); // 退出程序
-    }
     new->value = b;
     new->next = *first; // 原本指的next空間變成first原本指向的Null
     *first = new;       // new head 指到a1
@@ -53,15 +52,14 @@ void reverseList(struct node **first)
 }
 int main()
 {
-    int a;
     struct node *head = NULL;
-    printf("Print a list:");
     for (int i = 1; i < 6; i++)
     {
         push(i, &head);
     }
     reverseList(&head);
-    head = middleNode(head);
+    middleNode(&head);
+    printf("Print a list:");
     printList(head);
     return 0;
 }
