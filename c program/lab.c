2bee35023c95adab6bffe6dedc093386fc86aaa3
leo940625 <leo940625@gmail.com>
2024-06-03 21:56:36 +0800

    ss

--- c program/lab.c
+++ c program/lab.c
@@ -1,45 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-struct node{
-    char value[10];
-    struct node *next;
-};
-void reverseList(struct node **first){
-    struct node *buffer = NULL,*now = *first,*nextnode;
-    while (now != NULL)
-    {
-        nextnode = now->next;
-        now->next = buffer;
-        buffer = now;
-        now = nextnode;
-    }
-    *first = buffer;
-}
-void push(char a[],struct node **first){
-    struct node *new = malloc(sizeof (struct node)); // 重新取一個空間
-    if (new == NULL) {
-        printf("Memory allocation failed");
-        exit(1); // 退出程序
-    }
-    strcpy(new->value,a);
-    new -> next = *first; //原本指的next空間變成first原本指向的Null
-    *first = new; // new head 指到a1
-}
-void printList(struct node *a){
-    while (a != NULL)
-    {
-        printf("%s ",a->value);
-        a = a->next;
-    }
-}
 int main(){
-    struct node *head = NULL;
-    char c[10];
-    while (scanf("%s",c));
-    {
-        push(c,&head);
+    char a[20][100],b,ch;
+    int i = 0,j = 0;
+    while (scanf("%c", &ch) == 1 && ch != '\n') {
+        if (ch == ' ') {
+            a[i][j] = '\0';
+            i++;
+            j = 0;
+        } 
+        else {
+            a[i][j] = ch;
+            j++;
+        }
     }
-    //reverseList(&head);
-    printList(head);
+    b = a[i][j-1];
+    a[i][j-1] = '\0';
+    for (; i >= 0; i--)
+    {
+        j = 0;
+        while (a[i][j] != '\0' && a[i][j] != '?')
+        {
+            printf("%c",a[i][j]);
+            j++;
+        }
+        if(i > 0){
+            printf(" ");
+        }
+    }
+    printf("%c",b);
 }
