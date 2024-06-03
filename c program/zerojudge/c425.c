5db81850e9b88b46afd8a03e3b149e45057f82b8
leo940625 <leo940625@gmail.com>
2024-06-03 15:07:58 +0800

    ss

--- c program/zerojudge/c425.c
+++ c program/zerojudge/c425.c
@@ -3,4 +3,22 @@
 #include <stdlib.h>
 int main()
 {
+    int time,len;
+    scanf("%d",&time);
+    for (int i = 0; i < time; i++)
+    {
+        char *a,*b,*c,*temp;
+        a = malloc(100000*sizeof(char));
+        scanf("%s",a);
+        temp = realloc(a,(strlen(a))*sizeof(char));
+        a = temp; 
+        scanf("%d",&len);
+        b = malloc(len*sizeof(char));
+        strncpy(b,a,len);
+        b[len] = '\0';
+        c = malloc((strlen(a)+1)*sizeof(char));
+        strcpy(c,a+(len));
+        strcat(c,b);
+        printf("%s\n",c); /* code */
+    }
 }
