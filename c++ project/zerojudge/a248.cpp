15e001919c2d18e6f3d6126612c618457d3666d1
leo940625 <leo940625@gmail.com>
2024-06-11 08:58:43 +0800

    ss

---
+++ c++ project/zerojudge/a248.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{   
+  long long a,b,n;
+  int k,q;
+  while (cin >> a >> b >> n){
+    k = a/b;
+    if (n == 0)
+    {
+      cout << k ;
+    }
+    else
+    {
+      cout << k << '.';
+      a-=(k*b);
+      for (int i = 0; i < n; i++)
+      {
+        a *= 10;
+        q = a/b;
+        a -= (q*b);
+        cout << q;
+      }
+    }
+    cout << '\n';
+  }
+  if (cin.eof()) {
+      return 0;
+  } 
+}
+    
