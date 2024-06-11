6c9a11ad334f7605fc360962d198b8857aec9571
leo940625 <leo940625@gmail.com>
2024-06-11 08:34:49 +0800

    ss

--- c++ project/zerojudge/a215un.cpp
+++ c++ project/zerojudge/a215un.cpp
@@ -4,9 +4,8 @@ using namespace std;
 int main()
 {
   long long i,n,r,sum;
-  while (!cin.eof())
+  while ( cin >> n >> r)
   {
-    cin >> n >> r;
     sum = 0;
     for ( i = 1;; i++){
       sum += n+i-1;
@@ -16,5 +15,8 @@ int main()
     }
     cout << i <<'\n';
   }
+  if (cin.eof()) {
+    return 0;
+  } 
   return 0;
 }
