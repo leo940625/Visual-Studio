6102374f7a5d50967949656331363a4f1ac46777
leo940625 <leo940625@gmail.com>
2024-06-11 22:33:12 +0800

    ss

---
+++ c++ project/zerojudge/a148.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,k,sum;
+    float j;
+    while (cin >> n)
+    {
+        sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> k;
+            sum += k;
+        }
+        j = (float)sum/(float)n;
+        j > 59 ? cout << "no" << '\n': cout << "yes" << '\n';
+        if(cin.eof()){
+            return 0;
+        }
+    }
+}
---
+++ c++ project/zerojudge/a410.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, e, f;
+    cin >> a >> b >> c >> d >> e >> f;
+    if (a/d == b/e)
+    {
+        if (a/d == c/f)
+        {
+            cout << "Too many" <<'\n';
+            continue;
+        }
+        cout << "No answer" <<'\n';
+    }
+    else{
+        
+    }
+    
+    return 0;
+}
