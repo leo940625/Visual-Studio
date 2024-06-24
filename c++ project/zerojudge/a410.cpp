e746c02ef4c8990081856aeb715737ae32744c0b
leo940625 <leo940625@gmail.com>
2024-06-24 10:59:23 +0800

    ss

--- c++ project/zerojudge/a410.cpp
+++ c++ project/zerojudge/a410.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include<iomanip>
 using namespace std;
 
 int main()
 {
-    int a, b, c, d, e, f;
+    float a, b, c, d, e, f,x,y;
     cin >> a >> b >> c >> d >> e >> f;
     if (a/d == b/e)
     {
@@ -15,7 +16,10 @@ int main()
         }
     }
     else{
-        
+        y = (f*a/d-c)/(e*a/d-b);
+        x = (f*b/e-c)/(d*b/e-a);
+        cout << "x=" << fixed <<  setprecision(2) << x <<'\n';
+        cout << "y=" << fixed <<  setprecision(2) << y <<'\n';
     }
     return 0;
 }
