7323602789d7288c0083424a1c9968e25d76cca6
leo940625 <leo940625@gmail.com>
2024-09-10 22:48:03 +0800

    SS

--- c++ project/CA1/Prob2.cpp
+++ c++ project/CA1/Prob2.cpp
@@ -1,6 +1,16 @@
+/**
+* this code is to compute the dot product.
+*
+* @author аб?╔Э HSIEH WU-CHAO
+* @ID B12505023
+* @Department Engineering Science and Ocean Engineering
+* @Affiliation National Taiwan University
+*/
+
 #include <iostream>
 #include <math.h>
 using namespace std;
+
 bool isPrime(int a)
 {
     if (a <= 1) return false;
--- c++ project/CA1/dot.cpp
+++ c++ project/CA1/dot.cpp
@@ -1,3 +1,11 @@
+/**
+* this code is to compute the dot product.
+*
+* @author аб?╔Э HSIEH WU-CHAO
+* @ID B12505023
+* @Department Engineering Science and Ocean Engineering
+* @Affiliation National Taiwan University
+*/
 #include <iostream>
 using namespace std;
 
