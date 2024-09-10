863078c49f3a9c10e4f449e0e2bf0d9351dbb0d9
leo940625 <leo940625@gmail.com>
2024-09-10 22:38:53 +0800

    ww

---
+++ c++ project/CA1/Prob5.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+class rectangle {
+    private:
+        double width;
+        double length;
+        double perimeter;
+        double area;
+    public:
+        // Constructor, automatically compute the perimeter and area once it is OK.
+        rectangle (double wd, double lg): width(wd),length(lg),perimeter(2*(width+length)),area(width*length){};
+        // Set the dimension and automatically update the perimeter and area.
+        void setDimension (double wd, double lg){
+            width = wd;
+            length = lg;
+            perimeter = 2*(width+length);
+            area = width*length;
+        };
+        // Set the width and automatically update the perimeter and area.
+        void setWidth (double wd){
+            width = wd;
+            perimeter = 2*(width+length);
+            area = width*length;
+        };
+        // Set the length and automatically update the perimeter and area.
+        void setLength (double lg){
+            length = lg;
+            perimeter = 2*(width+length);
+            area = width*length;
+        };
+        // Return the width of the rectangle.
+        double getWidth (){
+            return width;
+        };
+        // Return the length of the rectangle.
+        double getLength (){
+            return length;
+        };
+        // Return the perimeter of the rectangle.
+        double getPerimeter (){
+            return perimeter;
+        };
+        // Return the area of the rectangle.
+        double getArea (){
+            return area;
+        };
+        // True, if width = length.
+        bool isSquare (){
+            int a;
+            a = width == length ? 1 : 0;
+            return a;
+        };
+};
