a937de1889e96534f4ab3c07633110328081e91b
leo940625 <leo940625@gmail.com>
2024-09-19 20:19:39 +0800

    ss

--- c++project/DS_CA1/CA1Prob2.cpp
+++ c++project/DS_CA1/CA1Prob2.cpp
@@ -13,8 +13,7 @@ using namespace std;
 
 bool isPrime(int a) // check whether a is a prime
 {
-    if (a <= 1)
-        return false;
+    if (a <= 1)return false;
     for (int i = 2; i < sqrt(a); i++)
     {
         if (a % i == 0)
