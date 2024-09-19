38ee50c7f9992558e46c577fcd8ccc59dbc95cde
leo940625 <leo940625@gmail.com>
2024-09-20 00:21:12 +0800

    ss

--- c++project/lab.cpp
+++ c++project/lab.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
 
-template<typename T>
-T add(T a, T b){
-    return a+b;
+template<typename T, typename G>
+G add(T a, T b){
+	G res = a+b;// G到底是甚麼型態?
+    return res;
 }
 int main(){
-    int int_a = 7022,int_b = 100;
     double d_a = 0.1, d_b = 7.022;
-    cout << add(int_a, int_b) << '\n';// 7122
-    cout << add(d_a, d_b) << '\n';// 7.122
+    cout << add<double, double>(d_a, d_b) << '\n';
+    cout << add<double, int>(d_a, d_b) << '\n';
+    cout << add<int, double>(d_a, d_b) << '\n';
 }
