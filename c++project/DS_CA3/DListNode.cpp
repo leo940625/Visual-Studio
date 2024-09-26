b64cbf4adb41265e7e6d03ec77b2db2068b9a4f6
leo940625 <leo940625@gmail.com>
2024-09-26 15:34:29 +0800

    ss

--- c++project/DS_CA3/DListNode.cpp
+++ c++project/DS_CA3/DListNode.cpp
@@ -1,6 +1,6 @@
-/**
- *  A DListNode is a node in a DList (doubly-linked list).
- */
+/*
+A DListNode is a node in a DList (doubly-linked list).
+*/
 
 #include "DListNode.h"
 
@@ -10,44 +10,9 @@
  *  @param p the node previous to this node.
  *  @param n thAAe node following this node.
  */
-template<typename T>
-DListNode<T>::DListNode(const T& i, DListNode<T>* p,DListNode<T>* n){
-	item = i;
-	prev = p;
-	next = n;
-}
+template class DListNode<int>;
+template class DListNode<double>;
 
-/**
- *  A DListNode is a node in a DList (doubly-linked list).
- */
-
-#include "DListNode.h"
-
-/**
- *  DListNode() constructor.
- *  @param i the item to store in the node.
- *  @param p the node previous to this node.
- *  @param n thAAe node following this node.
- */
-template<typename T>
-DListNode<T>::DListNode(const T& i, DListNode<T>* p,DListNode<T>* n){
-	item = i;
-	prev = p;
-	next = n;
-}
-
-/**
- *  A DListNode is a node in a DList (doubly-linked list).
- */
-
-#include "DListNode.h"
-
-/**
- *  DListNode() constructor.
- *  @param i the item to store in the node.
- *  @param p the node previous to this node.
- *  @param n thAAe node following this node.
- */
 template<typename T>
 DListNode<T>::DListNode(const T& i, DListNode<T>* p,DListNode<T>* n){
 	item = i;
