afe23158e5c6434f267aa46e0541c10ff18eea96
leo940625 <leo940625@gmail.com>
2024-10-15 00:41:09 +0800

    ss

--- c++project/DS_CA4/TestHashTable.cpp
+++ c++project/DS_CA4/TestHashTable.cpp
@@ -8,7 +8,7 @@
 #include"String.h"
 #include"Double.h"
 #include "CheckerBoard.h"
-//#include "CheckerBoard.cpp"
+#include "CheckerBoard.cpp"
 #include "Entry.h"
 using namespace std;
 
@@ -39,17 +39,14 @@ int main() {
     cout << "===============================Double Hash Table Test=====================================" << endl;
     HashTableChained<Double*, Integer*>* doubleTable = new HashTableChained<Double*, Integer*>(numBoards);
     initTable(doubleTable, numBoards);
-    //cout << "yes12" << endl;
     doubleTable->insert(new Double(10), new Integer(1));
-    //cout << "yes123" << endl;
     doubleTable->insert(new Double(20), new Integer(2));
-    //cout << "yes1234" << endl;
     cout << "Size should be 2 : " << doubleTable->size() << endl;
 
     cout << "===============================CheckerBoard Hash Table Test===============================" << endl;
     HashTableChained<CheckerBoard*, Integer*>* table = new HashTableChained<CheckerBoard*, Integer*>(numBoards);
     table->makeEmpty();
-    cout << "yes1234" << endl;
+    //cout << "yes1234" << endl;
     for (int i = 0; i < numBoards; i++) {
         CheckerBoard* board = randomBoard();
         table->insert(board, new Integer(i));
--- c++project/DS_CA4/remind.cpp
+++ c++project/DS_CA4/remind.cpp
@@ -1,1 +1,1 @@
-//需維修Board、Double的hashcode、compfunction
+//需維修Board、Double的hashcode、compfunction、checkboard hashcode
