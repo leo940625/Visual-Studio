b6483af6960d712366e187e8221759ba47cc7703
leo940625 <leo940625@gmail.com>
2024-10-28 11:39:26 +0800

    ss

--- c++project/lab.cpp
+++ c++project/lab.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
-#include <math.h>
+#include <fstream>
+#include <string>
+
 using namespace std;
-int main(){
-    int a[2][2] = {{4,5},{3,6}};
-    //cout << sum(a,1,1,2) << endl;
+
+void replaceSpacesAndSaveToFile(const string& inputStr, const string& fileName) {
+    string modifiedStr = inputStr;
+
+    // 將空格替換為逗號
+    for (size_t i = 0; i < modifiedStr.size(); ++i) {
+        if (modifiedStr[i] == ' ') {
+            modifiedStr[i] = ',';
+        }
+    }
+
+    // 創建並寫入 txt 文件
+    ofstream outFile(fileName);
+    if (outFile.is_open()) {
+        outFile << modifiedStr;
+        outFile.close();
+        cout << "已成功將結果保存至 " << fileName << " 文件中。" << endl;
+    } else {
+        cout << "無法創建文件。" << endl;
+    }
+}
+
+int main() {
+    string inputStr;
+    string fileName = "output.txt";
+
+    // 輸入字串
+    cout << "請輸入一個字串: ";
+    getline(cin, inputStr);
+
+    // 呼叫函式進行處理
+    replaceSpacesAndSaveToFile(inputStr, fileName);
+
+    return 0;
 }
