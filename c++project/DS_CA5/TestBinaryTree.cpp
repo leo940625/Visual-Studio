5b76db602cbb034b7c334abd365fcc4047f6e0c1
leo940625 <leo940625@gmail.com>
2024-11-19 16:51:08 +0800

    ss

--- c++project/DS_CA5/TestBinaryTree.cpp
+++ c++project/DS_CA5/TestBinaryTree.cpp
@@ -37,43 +37,42 @@ void testRemove(BinaryTree<Integer, String>* tree, int n, string shouldBe) {
 }
 
 int main() {
-  BinaryTree<Integer, String> *tree = new BinaryTree<Integer, String>();
-  cout << "Inserting 1A, 6V, 3K, 2Z, 5L, 9L:" << endl;
-  tree->insert(Integer(1), String("A"));
-  tree->insert(Integer(6), String("V"));
-  tree->insert(Integer(3), String("K"));
-  tree->insert(Integer(2), String("Z"));
-  tree->insert(Integer(5), String("L"));
-  tree->insert(Integer(9), String("L"));
-  cout << "The tree is:  " << tree->toString() << endl;
-  cout << "Size: " << tree->size() << endl; 
+    BinaryTree<Integer, String> *tree = new BinaryTree<Integer, String>();
+    cout << "Inserting 1A, 6V, 3K, 2Z, 5L, 9L:" << endl;
+    tree->insert(Integer(1), String("A"));
+    tree->insert(Integer(6), String("V"));
+    tree->insert(Integer(3), String("K"));
+    tree->insert(Integer(2), String("Z"));
+    tree->insert(Integer(5), String("L"));
+    tree->insert(Integer(9), String("L"));
+    cout << "The tree is:  " << tree->toString() << endl;
+    cout << "Size: " << tree->size() << endl; 
 
-  cout << "\nTesting find() ..." << endl;
-  testFind(tree, 1, "A");
-  testFind(tree, 9, "L");
-  testFind(tree, 5, "L");
-  testFind(tree, 4, "");
-  testFind(tree, 6, "V");
-  testFind(tree, 3, "K");
+    cout << "\nTesting find() ..." << endl;
+    testFind(tree, 1, "A");
+    testFind(tree, 9, "L");
+    testFind(tree, 5, "L");
+    testFind(tree, 4, "");
+    testFind(tree, 6, "V");
+    testFind(tree, 3, "K");
 
-  cout << "\nTesting remove() (for nodes with < 2 children) ..." << endl;
-  testRemove(tree, 5, "1A(((2Z)3K)6V(9L))");
-  testRemove(tree, 3, "1A((2Z)6V(9L))");
-  testRemove(tree, 1, "(2Z)6V(9L)");
-  tree->insert(Integer(7), String("S"));
-  tree->insert(Integer(8), String("X"));
-  tree->insert(Integer(10), String("B"));
-  cout << "After inserting 7S, 8X, 10B:  " << tree->toString() << endl;
-  cout << "Size:  " << tree->size() << endl;
-  if(tree->size() != 6)
+    cout << "\nTesting remove() (for nodes with < 2 children) ..." << endl;
+    testRemove(tree, 5, "1A(((2Z)3K)6V(9L))");
+    testRemove(tree, 3, "1A((2Z)6V(9L))");
+    testRemove(tree, 1, "(2Z)6V(9L)");
+    tree->insert(Integer(7), String("S"));
+    tree->insert(Integer(8), String("X"));
+    tree->insert(Integer(10), String("B"));
+    cout << "After inserting 7S, 8X, 10B:  " << tree->toString() << endl;
+    cout << "Size:  " << tree->size() << endl;
+    if(tree->size() != 6)
     cout << "  SHOULD BE 6." << endl;
-  
-  cout << "\nTesting remove() (for nodes with 2 children) ..." << endl;
-  testRemove(tree, 6, "(2Z)7S((8X)9L(10B))");
-  testRemove(tree, 9, "(2Z)7S((8X)10B)");
-  cout << "Size:  " << tree->size() << endl;
-  if(tree->size() != 4)
-    cout << "  SHOULD BE 4." << endl;
 
-  return 0;
+    cout << "\nTesting remove() (for nodes with 2 children) ..." << endl;
+    testRemove(tree, 6, "(2Z)7S((8X)9L(10B))");
+    testRemove(tree, 9, "(2Z)7S((8X)10B)");
+    cout << "Size:  " << tree->size() << endl;
+    if(tree->size() != 4)
+    cout << "  SHOULD BE 4." << endl;
+    return 0;
 }
