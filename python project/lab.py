8f14123c10d9d615f1a8d2bd33d48eba9e4ea205
leo940625 <leo940625@gmail.com>
2024-11-20 22:28:31 +0800

    ss

--- python project/lab.py
+++ python project/lab.py
@@ -1,17 +1,13 @@
-record_list = [[{'meal_type': 'Breakfast', 'food_type': 'Salad', 'order_place': 'Vendor'},
-                {'meal_type': 'Lunch', 'food_type': 'Burger', 'order_place': 'Vendor'},
-                {'meal_type': 'Dinner', 'food_type': 'Soup', 'order_place': 'Vendor'}],
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pddadad
+import gdown
 
-               [{'meal_type': 'Breakfast', 'food_type': 'Fried rice', 'order_place': 'Resturant'},
-                {'meal_type': 'Lunch', 'food_type': 'Burger', 'order_place': 'Vendor'}],
-
-               [{'meal_type': 'Breakfast', 'food_type': 'Pizza', 'order_place': 'Cooking at home'},
-                {'meal_type': 'Lunch', 'food_type': 'Salad', 'order_place': 'Cooking at home'},
-                {'meal_type': 'Dinner', 'food_type': 'Pizza', 'order_place': 'Vendor'}],
-
-               [{'meal_type': 'Breakfast', 'food_type': 'Noodle', 'order_place': 'Dining car'},
-                {'meal_type': 'Lunch', 'food_type': 'Fried rice', 'order_place': 'Vendor'},
-                {'meal_type': 'Dinner', 'food_type': 'Noodle', 'order_place': 'Vendor'}],
-                
-               [{'meal_type': 'Lunch', 'food_type': 'Pizza', 'order_place': 'Resturant'},
-                {'meal_type': 'Dinner', 'food_type': 'Burger', 'order_place': 'Cooking at home'}]]
+url = "https://drive.google.com/u/1/uc?id=1GVsoe5eOsH2f22rWWUEZu8ScDXcEFoDl"
+output = "udemy_dataset.csv"
+gdown.download(url, output)
+df = pd.read_csv(output)
+m, n = df.shape
+print(f"Size of dataset: {m}")
+print(f"Number of Features: {n}")
+df.head()
