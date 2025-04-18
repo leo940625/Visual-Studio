6c071cd36a307205d80c51ff3221f8e867528c61
leo940625 <leo940625@gmail.com>
2025-04-18 20:54:56 +0800

    ss

--- c++project/lab.cpp
+++ c++project/lab.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 int main(){
-    
+    double sum = 0;
+    for(int i = 0 ; i < 100000 ; i++){
+        sum += i*pow(1-0.00807,i-1)*0.00807;
+    }
+    cout << sum << endl;
 }
